bc0d44e4632042447be2c8e6451834b0b3397b6d
HooP <84915497+Phoritus@users.noreply.github.com>
2025-07-26 21:42:20 +0700

    Add files via upload

---
+++ assign 4/newtonRaphson.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) {
+    return pow(x,2) - 7;
+}
+
+double df(double x) {
+    return 2*x;
+}
+
+double newTonRaphson(double x, double error) {
+    double xi,current_error;
+    xi = x - (f(x) / df(x));
+    x = xi;
+    do {
+        xi = x - (f(x) / df(x));
+        cout << "X[i+1]: " << xi << endl;
+
+        current_error = fabs(xi - x);
+        cout << "current_error: " << current_error << endl;
+
+        x = xi;
+        cout << "X: " << x << endl;
+        cout << "--------------------" << endl;
+    } while (current_error > error);
+
+    return x;
+}
+
+int main(int argc, char *argv[]) {
+    cout << newTonRaphson(2, 1e-6);
+
+    return 0;
+
+}
